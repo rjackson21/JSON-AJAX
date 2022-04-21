19e5b47226f23fad34836a8272da1c93fcbb8342
Regina Jackson <rjackson156@gmail.com>
2022-04-20 20:58:01 -0400

    add eventListener for the button

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json')
-ourRequest.onload = function() {
-    var ourData = JSON.parse(ourRequest.responseText)
+const btn = document.getElementById("btn")
+btn.addEventListener("click" , function(){
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json')
+    ourRequest.onload = function() {
+        var ourData = JSON.parse(ourRequest.responseText)
+        console.log(ourData[0])
+    }
+    ourRequest.send()
+})
 
-    console.log(ourData[0])
-}
-ourRequest.send()
