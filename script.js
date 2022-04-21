cdd2bace9323968b882878edd9e619d3c2c8cc22
Regina Jackson <rjackson156@gmail.com>
2022-04-20 21:09:21 -0400

    create function to display the data when button is clicked

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const btn = document.getElementById("btn")
+const animalContainer = document.getElementById("animal-info")
+
 btn.addEventListener("click" , function(){
     var ourRequest = new XMLHttpRequest();
     ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json')
     ourRequest.onload = function() {
         var ourData = JSON.parse(ourRequest.responseText)
-        console.log(ourData[0])
+        renderHTML(ourData)
     }
     ourRequest.send()
 })
+
+function renderHTML(data){
+    var htmlString = ""
+
+    for (i = 0; i < data.length; i++){
+        htmlString += "<p>" + data[i].name + " is a " + data[i].species + "." + "</p>"
+    }
+
+    animalContainer.insertAdjacentHTML('beforeend', htmlString)
+}
+
 
